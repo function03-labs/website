28bbbdd8159c7aa07acaf6334dac1264ba1f7340
FindMalek <malekgarahellalbus@gmail.com>
2024-04-28 23:58:15 +0100

    feat(layout): added footer and header

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -36,20 +36,7 @@ import {
 } from "lucide-react"
 
 export type Icon = LucideIcon
-/*     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      {...props}
-    >
-      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5-10-5-10 5z"></path>
-    </svg> */
+
 function LogoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
