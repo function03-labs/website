cc6337083684197f5963ad19df22770e7e81bbcd
FindMalek <malekgarahellalbus@gmail.com>
2024-05-05 12:36:45 +0100

    fix(font): using 'display' instead of 'swap'

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { SiteFooter } from "@/components/layout/site-footer"
 
 const font = Noto_Serif({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
 })
 
 interface RootLayoutProps {
